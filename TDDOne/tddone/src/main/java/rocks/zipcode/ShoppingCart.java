ca8223103e210b0a5788daae2da47328c899dc58
Kristofer <kris@youngers.org>
2025-03-07 14:13:25 -0500

    Update ShoppingCart.java

--- TDDOne/tddone/src/main/java/rocks/zipcode/ShoppingCart.java
+++ TDDOne/tddone/src/main/java/rocks/zipcode/ShoppingCart.java
@@ -22,6 +22,14 @@ public class ShoppingCart {
     public void addItem(Item item) {
         items.add(item);
     }
+    // Item newItem = new Item(...);
+    // sc.addItem(newItem);
+    // Item expected = s.getItem(sc.getItemCount()-1);
+    // assertequal(expected, newItem);
+    
+    public Item getItem(int idx) {
+        return items.get(idx);
+    }
     
     public void removeItem(Item item) {
         items.remove(item);
